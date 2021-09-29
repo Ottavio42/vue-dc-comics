be54dca3acc9d304d16fbedb757bee2e5e6df002
Ottavio42 <ottaviog88@gmail.com>
2021-09-29 23:17:05 +0200

    struttura-footer-e-delete-helloworldVue

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+<footer>
+    <div class="footer-top">
+        <div class="foot-cont"></div>
+    </div>
+    <div class="footer-bot">
+        <div class="foot-cont"></div>
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../assets/style/variables";
+
+footer{
+    height: 20rem;
+    
+    .footer-top {
+        color: #fff;
+        height: 70%;
+    }
+    .footer-bot{
+            color: #fff;
+            background-color: #303030;
+            height: 30%;
+        }
+    .foot-cont{
+            width: $sizeContainer;
+        }
+
+}
+</style>
