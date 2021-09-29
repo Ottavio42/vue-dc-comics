bd2467036ff1d6acc6779a097b7a70f728557e27
Ottavio42 <ottaviog88@gmail.com>
2021-09-29 22:27:51 +0200

    content

---
+++ src/components/Content.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="content">
+    <div class="content-cont">
+        -- Content goes here --
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Content"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../assets/style/variables";
+
+.content{
+    height: 5rem;
+    background-color: #1c1c1c;
+    .content-cont{
+        font-size: 1.375rem;
+        color: #fff;
+        width: $sizeContainer;
+        margin: auto;
+    }
+}
+</style>
