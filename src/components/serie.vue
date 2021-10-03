4e9b9c9867cd1af9cd5f63d73055e0ed092f4279
Ottavio42 <ottaviog88@gmail.com>
2021-10-03 20:51:44 +0200

    fix-content

---
+++ src/components/serie.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="serie">
+		<img class="serie__image" :src="ser.thumb" :alt="ser.title">
+		<div class="product__name">
+			{{ser.title}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Serie",
+	props: ['ser']
+}
+</script>
+
+<style lang="scss" scoped>
+	.serie{
+		width: calc(100% / 6);
+		border: 2px solid #dbe9ff;
+		background-color: #fff;
+		padding: 25px 0;
+		.serie__name{
+			text-align: center;
+			font-weight: 500;
+			width: 60%;
+			margin: 0 auto;
+		}
+		.serie__image{
+			width: 50px;
+		}
+	}
